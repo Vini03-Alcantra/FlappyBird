d2c921921e47238358bc06e25f1d4b5b53589fcb
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-12-07 23:39:36 -0300

    criando método estaoSobrepostos

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -123,6 +123,16 @@ function Progresso(){
 //     passaro.animar()
 // }, 20)
 
+function estaoSobrepostos(elementoA, elementoB){
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+
 function FlappyBird(){
     let pontos = 0;
 
